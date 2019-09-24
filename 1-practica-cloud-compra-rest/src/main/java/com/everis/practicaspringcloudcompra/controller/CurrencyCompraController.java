536f5e92924f0b4590bafdc5edf68f4e9eff01f8
Alejandro <bazan.0720@gmail.com>
2019-09-24 14:15:22 -0500

    cree el controlador

---
+++ 1-practica-cloud-compra-rest/src/main/java/com/everis/practicaspringcloudcompra/controller/CurrencyCompraController.java
@@ -0,0 +1,9 @@
+package com.everis.practicaspringcloudcompra.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyCompraController {
+	
+	
+}
